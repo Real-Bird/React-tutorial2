f3277a48c329c5a4057ba7bd1a5e42ff353b3948
Real-Bird <marbil815@gmail.com>
2022-09-03 22:31:54 +0900

    22-09-03 code split

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import SampleContainer from "./components/SampleContainer";
+import loadable from "@loadable/component";
+import React, { Suspense } from "react";
+import { useState } from "react";
+
+// const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
-    <>
-      <SampleContainer />
-    </>
+    <div>
+      <header>
+        <div style={{ width: 200, height: 100, backgroundColor: "tan" }} />
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+      </header>
+      {/* <Suspense fallback={<div>loading...</div>}> */}
+      {visible && <SplitMe />}
+      {/* </Suspense> */}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from "./modules";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
